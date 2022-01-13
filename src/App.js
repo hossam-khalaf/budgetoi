49cb8a433f70bd90364fbdef8a54d002ce5483a0
hossam-khalaf <hossamhassan303089@gmail.com>
2022-01-13 07:17:26 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import { Container } from 'react-bootstrap'
+import { Button, Container, Stack } from 'react-bootstrap'
 
 function App() {
-	return <Container>Hey now</Container>
+	return (
+		<Container className='my-5'>
+			<Stack direction='horizontal' className='mb-5'>
+				<h1 className='me-auto' gap='2'>
+					Budgetoi
+				</h1>
+				<Button variant='secondary' className='me-2'>
+					Add Budget
+				</Button>
+				<Button variant='outline-secondary'>Add Expense</Button>
+			</Stack>
+		</Container>
+	)
 }
 
 export default App
