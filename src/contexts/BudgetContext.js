c023390b03b3e1ebc2e3f201ea648351c684ea10
hossam-khalaf <hossamhassan303089@gmail.com>
2022-01-28 20:08:14 +0400

    LocalStorage custom hook

--- src/contexts/BudgetContext.js
+++ src/contexts/BudgetContext.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 const BudgetsContext = React.createContext()
 
@@ -25,8 +26,8 @@ export function useBudgets() {
 */
 
 export const BudgetProvider = ({ children }) => {
-	const [budgets, setBudgets] = useState([])
-	const [expenses, setExpenses] = useState([])
+	const [budgets, setBudgets] = useLocalStorage('budgets', [])
+	const [expenses, setExpenses] = useLocalStorage('expenses', [])
 
 	const getBudgetExpenses = (budgetId) => {
 		return expenses.filter((expense) => expense.budgetId === budgetId)
